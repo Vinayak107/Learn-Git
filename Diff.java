478db122d36d0710776451e96565cbd05b88b09e
Vinayak Biradar <vinayakbiradar20@gmail.com>
2023-04-14 14:14:25 +0530

    added variable in Diff.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,8 @@ public class Diff {
     public static void main(String[] args) {
         int a=10;
         int b=10;
-        System.out.println(a-b);
+        int c=1;
+
+        System.out.println(a-b-c);
     }
 }
